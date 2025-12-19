3ac61d5c2ff753476d60c78782ebfbd9dfd2a4b5
shahadat <funkyd3v@gmail.com>
2025-12-19 15:18:44 +0600

    feat(backend):auth design changed

--- app/Providers/Filament/RecruiterPanelProvider.php
+++ app/Providers/Filament/RecruiterPanelProvider.php
@@ -2,24 +2,24 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Widgets\JobApplicantsChart;
-use App\Filament\Widgets\JobPostsChart;
-use App\Filament\Widgets\JobWidget;
-use App\Filament\Widgets\TotalJobs;
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\AuthenticateSession;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
+use Filament\Pages\Dashboard;
 use Filament\Support\Colors\Color;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
+use App\Filament\Widgets\TotalJobs;
+use App\Filament\Widgets\JobPostsChart;
+use Filament\Http\Middleware\Authenticate;
+use App\Filament\Widgets\JobApplicantsChart;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Filament\Http\Middleware\AuthenticateSession;
+use DiogoGPinto\AuthUIEnhancer\AuthUIEnhancerPlugin;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 class RecruiterPanelProvider extends PanelProvider
 {
@@ -49,6 +49,13 @@ class RecruiterPanelProvider extends PanelProvider
                 TotalJobs::class,
                 JobPostsChart::class,
                 JobApplicantsChart::class,
+            ])
+            ->plugins([
+                AuthUIEnhancerPlugin::make()
+                    ->emptyPanelBackgroundImageUrl('https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+                    ->showEmptyPanelOnMobile(false)
+                    ->emptyPanelBackgroundImageOpacity('70%')
+                    ->formPanelWidth('60%'),
             ])
             ->middleware([
                 EncryptCookies::class,

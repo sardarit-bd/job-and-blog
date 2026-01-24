6042c00cc5be75a90c38e6ac57f5c2d11e429708
shahadat <funkyd3v@gmail.com>
2026-01-24 14:35:56 +0600

    favicon added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,12 +33,9 @@
         </style>
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png"> -->
+        
         <link rel="icon" type="image/png" href="/favicon.png">
-<link rel="apple-touch-icon" href="/favicon.png">
+        <link rel="apple-touch-icon" href="/favicon.png">
 
 
         <link rel="preconnect" href="https://fonts.bunny.net">
